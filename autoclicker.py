6ea3aebcb934dfa082a0a5faa972d0a033662256
peimli <71698315+peimli@users.noreply.github.com>
2025-01-16 10:32:27 +0100

    Update autoclicker.py

--- autoclicker.py
+++ autoclicker.py
@@ -1,12 +1,34 @@
 import pyautogui
 import time
- 
+import threading
+import keyboard 
+
+
+stop_flag = False
+
 def klikk(): 
-    time.sleep(1)     
     pyautogui.click()
- 
+
 def a():
-    for i in range(50): 
+    global stop_flag
+    while not stop_flag:
         klikk()
- 
-a()
+        time.sleep(0.000000000000000000001)  
+
+def start_stop_clicking():
+    global stop_flag
+    if stop_flag:  
+        stop_flag = False
+        threading.Thread(target=a, daemon=True).start()
+        print("Started clicking...")
+    else:  
+        stop_flag = True
+        print("Stopped clicking.")
+
+
+keyboard.add_hotkey('s', start_stop_clicking)
+
+print("Press 's' to start/stop clicking.")
+
+
+keyboard.wait()
